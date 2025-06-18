8ff6d1258c58ed67ed2a7b3465c237e1ce3744c9
Leha <teffgog@gmail.com>
2025-06-19 00:41:32 +0300

    add repositories and abstractions

---
+++ src/CatalogService/CatalogService.Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using CatalogService.Domain.Abstractions.Repositories;
+using CatalogService.Domain.Entities;
+
+namespace CatalogService.Infrastructure.Repositories
+{
+    public class CategoryRepository(CatalogServiceDbContext context) : RepositoryBase<Category>(context), ICategoryRepository
+    {
+    }
+}
