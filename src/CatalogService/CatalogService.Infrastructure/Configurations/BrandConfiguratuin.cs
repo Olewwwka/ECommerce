4787be9ba11ff1e9d90f8d9b7e1f6b6c56aee67a
Leha <teffgog@gmail.com>
2025-06-19 00:40:50 +0300

    add dbcontext and configurations of models

---
+++ src/CatalogService/CatalogService.Infrastructure/Configurations/BrandConfiguratuin.cs
@@ -0,0 +1,22 @@
+﻿using CatalogService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CatalogService.Infrastructure.Configurations
+{
+    public class BrandConfiguratuin : IEntityTypeConfiguration<Brand>
+    {
+        public void Configure(EntityTypeBuilder<Brand> builder)
+        {
+            builder.HasKey(b => b.Id);
+
+            builder.Property(b => b.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(b => b.LogoUrl)
+                .IsRequired()
+                .HasMaxLength(255);
+        }
+    }
+}
