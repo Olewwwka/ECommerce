20ecf6ccb61d1fcd196231ee649f4a5b99c79b12
Leha <teffgog@gmail.com>
2025-06-29 19:05:40 +0300

    add project and configuration files

---
+++ src/CatalogService/CatalogService.Domain/Exceptions/CategoryMismatchException.cs
@@ -0,0 +1,10 @@
+﻿namespace CatalogService.Domain.Exceptions
+{
+    public class CategoryMismatchException : Exception
+    {
+        public CategoryMismatchException(string message) : base(message)
+        {
+
+        }
+    }
+}
