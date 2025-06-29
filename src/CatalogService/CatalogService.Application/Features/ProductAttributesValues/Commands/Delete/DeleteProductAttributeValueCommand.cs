c85b30e4c2137902f532f067dd836a99bc7054f6
Leha <teffgog@gmail.com>
2025-06-29 19:16:16 +0300

    add mapper, DTOs, and CQRS handlers for prodcut attrbiute values

---
+++ src/CatalogService/CatalogService.Application/Features/ProductAttributesValues/Commands/Delete/DeleteProductAttributeValueCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace CatalogService.Application.Features.ProductAttributesValues.Commands.Delete
+{
+    public record DeleteProductAttributeValueCommand(Guid ProductId, Guid ProductAttributeId) : IRequest<bool>
+    {
+    }
+}
---
+++ src/CatalogService/CatalogService.Application/Features/ProductAttributesValues/Queries/GetById/GetProductAttributeValueByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using CatalogService.Application.DTO.ProductAttributeValues;
+using MediatR;
+
+namespace CatalogService.Application.Features.ProductAttributesValues.Queries.GetById
+{
+    public record GetProductAttributeValueByIdQuery(Guid ProductId, Guid ProductAttributeId) : IRequest<ProductAttributeValueDto>
+    {
+    }
+}
