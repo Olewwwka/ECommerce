496e936f8da6a47e27dab0f8d9bc7270fc9aa37b
Aleksei Popovich <netvinkakk@gmail.com>
2025-08-13 12:13:04 +0300

    add ocelot api gateway

---
+++ ApiGateway/Program.cs
@@ -0,0 +1,36 @@
+using MMLib.SwaggerForOcelot.DependencyInjection;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+var configuration = builder.Configuration;
+
+builder.WebHost.UseUrls("http://localhost:5000");
+
+builder.Configuration
+    .AddJsonFile("Config/configuration.json", optional: false, reloadOnChange: true)
+    .AddJsonFile("Config/ocelot.swagger.json", optional: false, reloadOnChange: true);
+
+configuration.AddOcelotWithSwaggerSupport(options =>
+{
+    options.Folder = "Config";
+});
+
+
+services.AddOcelot(configuration);
+services.AddSwaggerForOcelot(builder.Configuration);
+
+services.AddAuthorization();
+
+var app = builder.Build();
+
+
+app.UseAuthorization();
+
+app.UseSwaggerForOcelotUI(options =>
+{
+    options.PathToSwaggerGenerator = "/swagger/docs";
+}).UseOcelot().Wait();
+
+app.Run();
